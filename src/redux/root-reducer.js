2a2b61b46559f88fd03802ac1a284a76ad9c8720
zedomaru <zakielyasadjauhari@gmail.com>
2020-01-20 07:40:28 -0800

    create remove item on checkout, add redux-persist for local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,12 +1,22 @@
 import {combineReducers} from 'redux'
+import {persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import userReducer from './user/user-reducer'
 import cartReducer from './cart/cart-reducer'
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 })
+
+export default persistReducer(persistConfig, rootReducer)
 
 // redux tips 101:
 // 1. create glue layer to wrap our whole application (using Provider from REACT-REDUX)
@@ -19,3 +29,5 @@ export default combineReducers({
 // CHAPTER 8 part 15 menit 10 sampe selesai
 // CHAPTER 8 part 16 CART UTILS
 // CHAPTER 8 part 20 & 22 redux selectors
+// CHAPTER 8 part 29 remove item passing
+// CHAPTER 8 part 30 decrease item logic
